f25c40cc96eeb7a62de49987d68ccd3c74b587c4
Angus Robertson <angusprobertson@gmail.com>
2021-01-07 16:13:14 +0000

    mouseclick event implementation

--- GameEngine/Board.java
+++ GameEngine/Board.java
@@ -5,9 +5,10 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.util.*;
 
-public class Board extends JPanel implements MouseListener{
+public class Board extends JPanel implements MouseListener, ActionListener, KeyListener{
 	Image darkSq = Toolkit.getDefaultToolkit().getImage("GameEngine/assets/dark_sq.png");
 	Image lightSq = Toolkit.getDefaultToolkit().getImage("GameEngine/assets/light_sq.png");
+	Image[] pieceImages = new Image[12];
 	boolean mouseDown = false;
 
 	// stores all information about game world
@@ -16,11 +17,38 @@ public class Board extends JPanel implements MouseListener{
 	int blKing = 4;
 	int whKing = 60;
 
+	int mx = 0;
+	int my = 0;
+	int mxLast = 0;
+	int myLast = 0;
+	int pieceHeld = 0;
+	int pieceHeldFrom = 0;
+
+
 
 	public Board(){
 		//constructor initiates game pieces
+		javax.swing.Timer t = new javax.swing.Timer(10, this);
+		t.start();
+
+		addKeyListener(this);
+		addMouseListener(this);
+		setFocusTraversalKeysEnabled(true);
 		generatePieces();
 		repaint();
+
+		pieceImages[0] = Toolkit.getDefaultToolkit().getImage("GameEngine/assets/king_black.png");
+		pieceImages[1] = Toolkit.getDefaultToolkit().getImage("GameEngine/assets/rook_black.png");
+		pieceImages[2] = Toolkit.getDefaultToolkit().getImage("GameEngine/assets/knight_black.png");
+		pieceImages[3] = Toolkit.getDefaultToolkit().getImage("GameEngine/assets/bishop_black.png");
+		pieceImages[4] = Toolkit.getDefaultToolkit().getImage("GameEngine/assets/queen_black.png");
+		pieceImages[5] = Toolkit.getDefaultToolkit().getImage("GameEngine/assets/pawn_black.png");
+		pieceImages[6] = Toolkit.getDefaultToolkit().getImage("GameEngine/assets/king_white.png");
+		pieceImages[7] = Toolkit.getDefaultToolkit().getImage("GameEngine/assets/rook_white.png");
+		pieceImages[8] = Toolkit.getDefaultToolkit().getImage("GameEngine/assets/knight_white.png");
+		pieceImages[9] = Toolkit.getDefaultToolkit().getImage("GameEngine/assets/bishop_white.png");
+		pieceImages[10] = Toolkit.getDefaultToolkit().getImage("GameEngine/assets/queen_white.png");
+		pieceImages[11] = Toolkit.getDefaultToolkit().getImage("GameEngine/assets/pawn_white.png");
 	}
 
 	public void paint(Graphics g){
@@ -37,32 +65,56 @@ public class Board extends JPanel implements MouseListener{
 		drawPieces(g);
 	}
 
+	//overridden keyEvent methods
+	public void keyPressed(KeyEvent e){
+		if(e.getKeyCode() == KeyEvent.VK_ESCAPE){
+			System.exit(0);
+		}
+		System.out.println("hello");
+	}
+	public void keyReleased(KeyEvent e){}
+	public void keyTyped(KeyEvent e){
+		if(e.getKeyCode() == KeyEvent.VK_ESCAPE){
+			System.exit(0);
+		}
+	}
+
 	//overridden mouseEvent methods
 	public void mouseExited(MouseEvent e){}
 	public void mouseEntered(MouseEvent e){}
+	public void mouseClicked(MouseEvent e){}
 	public void mousePressed(MouseEvent e){
-		mouseDown = true;
+		if(!mouseDown){
+			int clickPosX = e.getX();
+			int clickPosY = e.getY();
+			for(int i = 0; i<64; i++){
+				boolean inXCol = clickPosX >= 2+64*(i%8)	  && clickPosX <= 66+64*(1+(i%8) );
+				boolean inYCol = clickPosY >= 2+64*(int)(i/8) && clickPosY <= 66+64*(int)(i/8);
+				//System.out.println(clickPosX + " : " + clickPosY);
+				//System.out.println(2+64*(i%8));
+				//System.out.println(66+64*(i%8));
 
+				if(inXCol && inYCol){
+					System.out.println(getPieceIdFromSquare(i));
+				}
+			}
+		}
+		mouseDown = true;
 	}
 	public void mouseReleased(MouseEvent e){
 		mouseDown = false;
 	}
-	public void mouseClicked(MouseEvent e){}
+
+	//overridden actionEvent methods
+	public void actionPerformed(ActionEvent e){
+		mxLast = mx;
+		myLast = my;
+		mx = MouseInfo.getPointerInfo().getLocation().x;
+		my = MouseInfo.getPointerInfo().getLocation().y;
+		repaint();
+	}
 
 	public void drawPieces(Graphics g){
-		Image[] pieceImages = new Image[12];
-		pieceImages[0] = Toolkit.getDefaultToolkit().getImage("GameEngine/assets/king_black.png");
-		pieceImages[1] = Toolkit.getDefaultToolkit().getImage("GameEngine/assets/rook_black.png");
-		pieceImages[2] = Toolkit.getDefaultToolkit().getImage("GameEngine/assets/knight_black.png");
-		pieceImages[3] = Toolkit.getDefaultToolkit().getImage("GameEngine/assets/bishop_black.png");
-		pieceImages[4] = Toolkit.getDefaultToolkit().getImage("GameEngine/assets/queen_black.png");
-		pieceImages[5] = Toolkit.getDefaultToolkit().getImage("GameEngine/assets/pawn_black.png");
-		pieceImages[6] = Toolkit.getDefaultToolkit().getImage("GameEngine/assets/king_white.png");
-		pieceImages[7] = Toolkit.getDefaultToolkit().getImage("GameEngine/assets/rook_white.png");
-		pieceImages[8] = Toolkit.getDefaultToolkit().getImage("GameEngine/assets/knight_white.png");
-		pieceImages[9] = Toolkit.getDefaultToolkit().getImage("GameEngine/assets/bishop_white.png");
-		pieceImages[10] = Toolkit.getDefaultToolkit().getImage("GameEngine/assets/queen_white.png");
-		pieceImages[11] = Toolkit.getDefaultToolkit().getImage("GameEngine/assets/pawn_white.png");
 		for(int p = 0; p<12; p++){
 			if(p != 0 && p!= 6){
 				for(int i = 0; i<64; i++){
@@ -74,6 +126,16 @@ public class Board extends JPanel implements MouseListener{
 		}
 		g.drawImage(pieceImages[0], 2+(blKing%8)*64, 2+(int)(blKing/8)*64, this);
 		g.drawImage(pieceImages[6], 2+(whKing%8)*64, 2+(int)(whKing/8)*64, this);
+	}
+
+	public int getPieceIdFromSquare(int n){
+		int result = -1;
+		for(int i = 1; i<12; i++){
+			if(bitBoard[i][n] && i != 6){
+				result = i;
+			}
+		}
+		return result;
 	}
 
 	public void generatePieces(){
@@ -104,7 +166,6 @@ public class Board extends JPanel implements MouseListener{
 		bitBoard[9][61] = true;
 		bitBoard[4][3] = true; //queens
 		bitBoard[10][59] = true;
-
 
 	}
 
--- GameEngine/Main.java
+++ GameEngine/Main.java
@@ -7,9 +7,10 @@ public class Main{
 	public static void main(String[] args){
 		//Create Window to open chess game, instantiate Board and add to window
 		JFrame chessWindow = new JFrame("ChessAI");
-		chessWindow.setBounds(400, 100, 512, 534);
+		chessWindow.setBounds(400, 100, 512, 512);
 		chessWindow.setExtendedState(JFrame.NORMAL);
 		chessWindow.setResizable(false);
+		chessWindow.setUndecorated(true);
 		chessWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Board board = new Board();
 		chessWindow.add(board);
